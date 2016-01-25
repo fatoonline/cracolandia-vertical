efde722e7cd7ca502498d889fac826fdac5fe86e
fatoonline <social@fatoonline.com.br>
2016-01-25 13:04:19 -0200

    criação JS

--- pessoal.js
+++ pessoal.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-	
+	//INICO
+	$('#inicio').click(function() {
+		$('body,html').animate({scrollTop: $(".header").offset().top}, "slow");
+	});
+
+	//cracolandia
+	$('#cracolandia').click(function() {
+		$('body,html').animate({scrollTop: $(".cracolandia-vertical").offset().top}, "slow");
+	});
+
+	//DRAMA
+	$('#drama').click(function() {
+		$('body,html').animate({scrollTop: $(".drama").offset().top}, "slow");
+	});
+
 });
