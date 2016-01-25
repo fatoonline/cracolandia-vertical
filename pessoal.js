7d3feda3c373c2ae847fdadc760f04df61f431ea
fatoonline <social@fatoonline.com.br>
2016-01-25 14:14:35 -0200

    correcao video

--- pessoal.js
+++ pessoal.js
@@ -14,4 +14,19 @@ $(document).ready(function(){
 		$('body,html').animate({scrollTop: $(".drama").offset().top}, "slow");
 	});
 
+	//SIDE MENU
+	$('#btn-side-menu').click(function() {
+		$(this).animate({"margin-left":"-80px;"}, "slow");
+		$('.side-menu').animate({
+			'margin-left':'0px'
+		}, 'slow');
+	});
+
+	$('#fechar').click(function() {
+		(this).animate({"margin-left":"0px"}, "slow");
+		$('.side-menu').animate({
+			'margin-left':'-220px'
+		}, 'slow');
+	});
+
 });
