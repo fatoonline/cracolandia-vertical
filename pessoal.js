3db0ca7337853fc931e274a8050913ed6a7926c5
fatoonline <social@fatoonline.com.br>
2016-01-25 14:44:11 -0200

    correção js

--- pessoal.js
+++ pessoal.js
@@ -16,14 +16,14 @@ $(document).ready(function(){
 
 	//SIDE MENU
 	$('#btn-side-menu').click(function() {
-		$(this).animate({"margin-left":"-80px;"}, "slow");
+		$("#btn-side-menu").animate({"margin-left":"-80px;"}, "slow");
 		$('.side-menu').animate({
 			'margin-left':'0px'
 		}, 'slow');
 	});
 
 	$('#fechar').click(function() {
-		$(this).animate({"margin-left":"0px"}, "slow");
+		$("#btn-side-menu").animate({"margin-left":"0px"}, "slow");
 		$('.side-menu').animate({
 			'margin-left':'-220px'
 		}, 'slow');
